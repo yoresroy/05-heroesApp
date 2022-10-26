99fa9f07978048fd02ba91397ccb96d9d233f602
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-10-26 18:26:39 -0500

    ADD - componentchild

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,10 +4,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const rutas : Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
