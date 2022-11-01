3a22d238c781280d98e9aa9ef2117a0cb8637982
yoresroy <yor_27@hotmail.com>
2022-11-01 00:41:43 -0600

    WIP - mostrar carga

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe!:Heroe;
+
+  constructor(private activatedRoute: ActivatedRoute, private heroesService:HeroesService) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.pipe(
+      switchMap( ({sour}) => { 
+        console.log(sour)
+        return new Observable<string>(sour) 
+      } )
+    ).subscribe( res => {
+      console.log(res)
+    });
+
+    /*
+    this.activatedRoute.params.subscribe( ({id}) => {
+      console.log(id);
+      
+    });
+    */
+    
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroes.interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,12 @@ export class HeroesService {
 
   constructor( private http: HttpClient ) { }
 
-  getHeroes(){
+  getHeroes(): Observable<Heroe[]>{
     return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+  }
+
+  getHeroe(id:string):Observable<Heroe>{
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
   }
 
 }
