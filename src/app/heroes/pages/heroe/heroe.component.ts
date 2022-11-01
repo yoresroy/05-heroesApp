16ecf8e7f99bae1b4e704767a3b337f4c9d0ed7b
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-11-01 09:41:49 -0600

    Add - Consulta de heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Heroe } from '../../interfaces/heroes.interfaces';
 import { HeroesService } from '../../services/heroes.service';
-import { switchMap, Observable } from 'rxjs';
+import { switchMap, Observable, tap } from 'rxjs';
 
 
 @Component({
@@ -18,21 +18,11 @@ export class HeroeComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.pipe(
-      switchMap( ({sour}) => { 
-        console.log(sour)
-        return new Observable<string>(sour) 
-      } )
-    ).subscribe( res => {
-      console.log(res)
-    });
-
-    /*
-    this.activatedRoute.params.subscribe( ({id}) => {
-      console.log(id);
-      
-    });
-    */
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroe(id) )
+    )
+    .subscribe( heroe => this.heroe = heroe ); 
     
   }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 
@@ -18,7 +21,9 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
   ]
 })
 export class MaterialModule { }
