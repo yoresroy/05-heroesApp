ca728e34e7a7701ba12544bd278fe284e7700a05
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-11-15 18:23:33 -0600

    ADD - manejo de flujo

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+
+  login() {
+    
+    //Ir Backend
+    //Un usuario
+    this.router.navigate(['./heroes']);
+
+
   }
+
 
 }
